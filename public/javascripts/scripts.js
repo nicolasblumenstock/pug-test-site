a54a298c6dc9d57045221a9e19658592b77b0bad
Nicolas Blumenstock <nicpblum@gmail.com>
2017-06-05 08:38:56 -0400

    testing on mobile

---
+++ public/javascripts/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(()=>{
+	$(".rand-rest").on('tap', function(){
+		$('.rand-rest').addClass('flip');
+	});
+
+	$('.rand-rest').click(function(){
+		$('.rand-rest').addClass('flip');
+	})
+})
