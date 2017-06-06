b7fbd86b658ebb8eccc5051112ea71826ec52419
Nicolas Blumenstock <nicpblum@gmail.com>
2017-06-06 15:48:04 -0400

    minor changes

--- public/javascripts/test-scripts.js
+++ public/javascripts/test-scripts.js
@@ -1,9 +1,6 @@
 $(document).ready(function(){
 	$('.section').on('click', function(event){
 		$('#mySideNav').width('250px');
-		$('#site').css({
-			'margin-left': '250px'
-		})
 	})
 	
 	$('#site').fullpage({
@@ -12,9 +9,6 @@ $(document).ready(function(){
 
 	$('.closebtn').on('click', function(event){
 		$('#mySideNav').width('0px');
-		$('#site').css({
-			'margin-left': '0px'
-		})
 	})
 
 });
