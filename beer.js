2a44759e3c89c4621429af883094534305c6b0b7
Chris Boeckel <cboe07@gmail.com>
2017-06-07 15:17:25 -0400

    -

--- beer.js
+++ beer.js
@@ -3,13 +3,10 @@ var base = "http://api.brewerydb.com/v2"
 
 // document.addEventListener('DOMContentLoaded', bind); //DOM must load first before binding buttons.
 
-function bind(){
-    var el = document.getElementById('beerSub');
-    el.addEventListener('click', function(event){
-        var beerName = document.getElementById('br').value;
-        var bts = beerName.split(' ').join('_');
-        var type = "&type=beer";
-        var key = config.apiKey;
-        var search = "/search?q=";
-        var fullUrl = base + search + bts + type + key;
-    });
+var beerName = document.getElementById(??????).value;
+var bts = beerName.split(' ').join('_');
+var type = "&type=beer";
+var key = config.apiKey;
+var search = "/search?q=";
+var fullUrl = base + search + bts + type + key;
+   
