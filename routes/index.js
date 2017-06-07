c783e8e868bcbdf7d0784b1bc2546e28f52eed50
Nicolas Blumenstock <nicpblum@gmail.com>
2017-06-07 15:46:39 -0400

    added .gets for all pages, added requires for new modules

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var config = require('../config/config')
 var mysql = require('mysql');
+var bcrypt = require('bcrypt-nodejs');
+var connection = mysql.createConnection({
+	host: config.sql.host,
+	user: config.sql.user,
+	password: config.sql.password,
+	database: config.sql.database
+})
+
+connection.connect();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,6 +23,26 @@ router.get('/login', (req,res)=>{
 
 router.get('/register', (req,res)=>{
 	res.send('REGISTRATION PAGE')
+});
+
+router.get('/account', (req,res)=>{
+	res.render('/account', {})
+})
+
+router.get('/restaurants', (req,res)=>{
+	res.render('/restaurants', {})
+})
+
+router.get('/recipes', (req,res)=>{
+	res.render('/recipes', {})
+})
+
+router.get('/beverages', (req,res)=>{
+	res.render('/beverages', {})
+})
+
+router.get('/contact', (req,res)=>{
+	res.render('/contact', {})
 })
 
 module.exports = router;
