e83ad9b7bb8783e307f1c2384b24cd0dd40bba5c
Chris Boeckel <cboe07@gmail.com>
2017-06-08 10:01:28 -0400

    Update wine data

--- config/config.js
+++ config/config.js
@@ -1,11 +1,13 @@
 var config = {
-	apiKey: '&key=dd1140de5619a72696d1f9773333c85e',
+	beerKey: '&key=dd1140de5619a72696d1f9773333c85e',
 	sql: {
 		host: '127.0.0.1',
 		user: 'x',
 		password: 'x',
 		database: 'bytesandbrews'
-	}	
+	},	
+
+	wineKey: '?akey=8jncs6kpdwlsv2gkd24zqiyadfzhi0b07ybsajsldrssfgpg',
 }
 
 module.exports = config;
--- routes/index.js
+++ routes/index.js
@@ -45,4 +45,59 @@ router.get('/contact', (req,res)=>{
 	res.render('contact', {})
 })
 
+
+
+
+
+////// Snooth
+const snoothBaseUrl = "http://api.snooth.com/wines/";
+const snoothBaseUrl2 = 'http://api.snooth.com/wine/';
+const wineKey = config.wineKey;
+const ip = '66.28.234.115';
+
+
+var wineToSearch = wineName.split(' ').join('+');
+
+
+// What color wine do you like? Red, white, rose, amber, clear? Get our preferences?
+var wineColor = 't='+ colorSelected
+var snoothTypeUrl = snoothTypeUrl + wineKey + ip + wineColor;
+
+
+// Found a good wine? Input a particular wine and get recipes
+
+
+// router.get('/beverages', function(req, res, next) {
+// 	request.get(,(error, response, wineData)=>{
+// 		var wineData = JSON.parse(wineData);
+// 		res.render('beverages', { 
+// 			wineName: wine[i].recipes.name,
+// 			wineImage: wine[i].recipes.image,
+// 			wineRecipeLink: wine[i].recipes.link
+
+// 			});
+
+// 	});
+
+// });
+
+router.post('/beverages', (req,res)=>{
+	// req.body is availbale because of the body-parser module
+	// req.body is where POSTED data will live
+	// res.json(req.body);
+	var wineId = wineName.split(' ').join('-');;
+	var snoothRecipeUrl = snoothBaseUrl2 + wineKey + ip + wineId + '&food=1';
+	request.get(snoothRecipeUrl2,(error, response, wineData)=>{
+		// res.json(JSON.parse(wineData));
+		var wineData = JSON.parse(wineData);
+		res.render('beverages', { 
+			wineName: wine[i].recipes.name,
+			wineImage: wine[i].recipes.image,
+			wineRecipeLink: wine[i].recipes.link
+
+		});
+	});
+
+});
+
 module.exports = router;
