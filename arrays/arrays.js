d5c09ee9cf4071ebc6f1926a6ccf1b40be1ee9a8
Nicolas Blumenstock <nicpblum@gmail.com>
2017-06-09 09:06:27 -0400

    made arrays for yummly info

---
+++ arrays/arrays.js
@@ -0,0 +1,69 @@
+var arrays = {
+	cuisines: [
+		'American',
+		'Italian',
+		'Asian',
+		'Mexican',
+		'French',
+		'Southwestern',
+		'Barbecue',
+		'Indian',
+		'Chinese',
+		'English',
+		'Mediterranean',
+		'Greek',
+		'Spanish',
+		'German',
+		'Thai',
+		'Moroccan',
+		'Irish',
+		'Japanese',
+		'Cuban',
+		'Hawaiin',
+		'Swedish',
+		'Hungarian',
+		'Portugese'
+	],
+	dietsSearch: [
+		'388^Lacto vegetarian',
+		'389^Ovo vegetarian',
+		'390^Pescetarian',
+		'386^Vegan',
+		'387^Lacto-ovo vegetarian',
+		'403^Paleo'
+	],
+	diets: [
+		'Lacto vegetarian',
+		'Ovo vegetarian',
+		'Pescetarian',
+		'Vegan',
+		'Lacto-ovo vegetarian',
+		'Paleo'
+	],
+	allergiesSearch: [
+		'393^Gluten-Free',
+		'394^Peanut-Free',
+		'398^Seafood-Free',
+		'399^Sesame-Free',
+		'400^Soy-Free',
+		'396^Dairy-Free',
+		'397^Egg-Free',
+		'401^Sulfite-Free',
+		'395^Tree Nut-Free',
+		'392^Wheat-Free'
+	],
+	allergies: [
+		'Gluten-Free',
+		'Peanut-Free',
+		'Seafood-Free',
+		'Sesame-Free',
+		'Soy-Free',
+		'Dairy-Free',
+		'Egg-Free',
+		'Sulfite-Free',
+		'Tree Nut-Free',
+		'Wheat-Free'
+	]
+};
+
+module.exports = arrays;
