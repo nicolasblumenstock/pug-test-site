cc0938f42a92a8c12e4b1af9530d613cc00e94a5
Hayes Capers <capersj5@yahoo.com>
2017-06-09 08:52:51 -0400

    Recipe css and horizontal slides

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,16 @@ var router = express.Router();
 var config = require('../config/config')
 var mysql = require('mysql');
 var bcrypt = require('bcrypt-nodejs');
+var request = require('request');
 var connection = mysql.createConnection({
 	host: config.sql.host,
 	user: config.sql.user,
 	password: config.sql.password,
 	database: config.sql.database
-})
+});
+
+var yummlyCreds = `_app_id=${config.yummly.id}&_app_key=${config.yummly.key}&`
+var baseYummlyMultiSearchUrl = `https://api.yummly.com/v1/api/recipes?${yummlyCreds}`
 
 connection.connect();
 
@@ -260,6 +264,25 @@ router.get('/restaurants', (req,res)=>{
 router.get('/recipes', (req,res)=>{
 	res.render('recipes', {})
 })
+
+router.post('/random-recipe', (req,res)=>{
+	var randomNumForSearch = Math.floor(Math.random() * 2000);
+	var searchResults = `&maxResult=5&start=${randomNumForSearch}`
+	var dinnerParam = '&allowedCourse[]=course^course-Main%20Dishes'
+	var requirePics = '&requirePictures=true'
+	var searchUrl = baseYummlyMultiSearchUrl + searchResults + dinnerParam + requirePics;
+	// console.log(searchUrl);
+
+	request.get(searchUrl, (error,response,data)=>{
+		if (error) throw error;
+		var data = JSON.parse(data);
+		// console.log(recipeData.matches[0].recipeName)
+		var recipeData = data.matches
+		res.render('recipes', {
+			recipes: recipeData
+		});
+	});
+});
 
 router.get('/beverages', (req,res)=>{
 	res.render('beverages', {})
